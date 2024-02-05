d874838a8ce8fa0288115b3135983d2c7b349507
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2024-02-05 14:45:40 -0600

    Fixing build

--- embedded/servlet-server/src/main/java/examples/TimeServlet.java
+++ embedded/servlet-server/src/main/java/examples/TimeServlet.java
@@ -15,10 +15,11 @@ package examples;
 
 import java.io.IOException;
 import java.util.Date;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @SuppressWarnings("serial")
 @WebServlet(urlPatterns = {"/time"})
