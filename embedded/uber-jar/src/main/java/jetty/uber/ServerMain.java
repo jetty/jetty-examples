da440184b7d2afe360890a46a3255078eaa7210d
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2024-04-15 16:07:21 -0500

    Adding embedded/uber-jar example

---
+++ embedded/uber-jar/src/main/java/jetty/uber/ServerMain.java
@@ -0,0 +1,76 @@
+//
+// ========================================================================
+// Copyright (c) 1995 Mort Bay Consulting Pty Ltd and others.
+//
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License v. 2.0 which is available at
+// https://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+// which is available at https://www.apache.org/licenses/LICENSE-2.0.
+//
+// SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+// ========================================================================
+//
+
+package jetty.uber;
+
+import java.net.URI;
+import java.net.URL;
+import javax.websocket.server.ServerContainer;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.DefaultServlet;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.util.resource.Resource;
+import org.eclipse.jetty.websocket.jsr356.server.deploy.WebSocketServerContainerInitializer;
+
+public class ServerMain
+{
+    public static void main(String[] args) throws Throwable
+    {
+
+        try
+        {
+            new ServerMain().run();
+        }
+        catch (Throwable t)
+        {
+            t.printStackTrace();
+        }
+    }
+
+    public void run() throws Exception
+    {
+        Server server = new Server(8080);
+
+        URL webRootLocation = this.getClass().getResource("/webroot/index.html");
+        if (webRootLocation == null)
+        {
+            throw new IllegalStateException("Unable to determine webroot URL location");
+        }
+
+        URI webRootUri = URI.create(webRootLocation.toURI().toASCIIString().replaceFirst("/index.html$","/"));
+        System.err.printf("Web Root URI: %s%n",webRootUri);
+
+        ServletContextHandler context = new ServletContextHandler();
+        context.setContextPath("/");
+        context.setBaseResource(Resource.newResource(webRootUri));
+        context.setWelcomeFiles(new String[] { "index.html" });
+
+        context.getMimeTypes().addMimeMapping("txt","text/plain;charset=utf-8");
+
+        server.setHandler(context);
+
+        // Add WebSocket endpoints
+        ServerContainer wsContainer = WebSocketServerContainerInitializer.configureContext(context);
+        wsContainer.addEndpoint(TimeSocket.class);
+
+        // Add Servlet endpoints
+        context.addServlet(TimeServlet.class,"/time/");
+
+        context.addServlet(DefaultServlet.class,"/");
+
+        // Start Server
+        server.start();
+        server.join();
+    }
+}
