dc062ec618fdd099d0d04a70a17bee48078df3f7
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2024-04-23 16:49:01 -0500

    Updating for 11.0.x

--- embedded/logging-java-util-logging/src/main/java/examples/HelloHandler.java
+++ embedded/logging-java-util-logging/src/main/java/examples/HelloHandler.java
@@ -15,10 +15,10 @@ package examples;
 
 import java.io.IOException;
 import java.util.logging.Logger;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 
--- embedded/logging-mixed/src/main/java/examples/HelloJULServlet.java
+++ embedded/logging-mixed/src/main/java/examples/HelloJULServlet.java
@@ -15,10 +15,11 @@ package examples;
 
 import java.io.IOException;
 import java.util.logging.Logger;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @SuppressWarnings("serial")
 public class HelloJULServlet extends HttpServlet
