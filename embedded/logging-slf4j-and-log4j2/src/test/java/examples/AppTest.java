8cb434d53dec03d8fbb62dee167da8e68aac95a7
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2024-04-23 16:22:12 -0500

    Adding embedded/logging-* examples

---
+++ embedded/logging-slf4j-and-log4j2/src/test/java/examples/AppTest.java
@@ -0,0 +1,85 @@
+//
+// ========================================================================
+// Copyright (c) 1995 Mort Bay Consulting Pty Ltd and others.
+//
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License v. 2.0 which is available at
+// https://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+// which is available at https://www.apache.org/licenses/LICENSE-2.0.
+//
+// SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+// ========================================================================
+//
+
+package examples;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URI;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.util.IO;
+import org.eclipse.jetty.util.component.LifeCycle;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class AppTest
+{
+    private static Server server;
+    private static URI serverUri;
+
+    @BeforeEach
+    public void startServer() throws Exception
+    {
+        LoggingUtil.config();
+
+        server = new Server();
+        ServerConnector connector = new ServerConnector(server);
+        connector.setPort(0); // let it use whatever port thats free
+        server.addConnector(connector);
+
+        // add handler
+        server.setHandler(new HelloHandler("Hello Basic"));
+
+        // Start server
+        server.start();
+
+        // Establish the Server URI
+        String host = connector.getHost();
+        if (host == null)
+        {
+            host = "localhost";
+        }
+        int port = connector.getLocalPort();
+        serverUri = new URI(String.format("http://%s:%d/",host,port));
+    }
+
+    @AfterEach
+    public void stopServer()
+    {
+        LifeCycle.stop(server);
+    }
+
+    @Test
+    public void testGet() throws IOException
+    {
+        URI getURI = serverUri.resolve("/");
+
+        String loggername = "examples";
+
+        HttpURLConnection connection = (HttpURLConnection)getURI.toURL().openConnection();
+        assertThat("Connection.statusCode",connection.getResponseCode(),is(HttpURLConnection.HTTP_OK));
+        try (InputStream in = connection.getInputStream())
+        {
+            String response = IO.toString(in);
+            assertThat("response",response,containsString("Hello"));
+        }
+    }
+}
